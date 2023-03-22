9685e8636ab382c8d5b6dc4597b09079145f2b93
zocom-christoffer-wallenberg <54267140+zocom-christoffer-wallenberg@users.noreply.github.com>
2023-03-22 15:01:58 +0100

    Added code

--- react-router-example/src/views/Account.jsx
+++ react-router-example/src/views/Account.jsx
@@ -1,13 +1,14 @@
 import { useLocation } from "react-router-dom"
 
 function Account() {
-    const navigationState = useLocation();
+    const navigationState = useLocation(); // Hämtar från vårt "state" som vi skickade från Registration.jsx
     console.log(navigationState);
 
     return (
       <section>
         <p>Account</p>
-        { navigationState?.state?.user ?
+        { /** Här använder vi oss av en tenary operator för att enbart visa något om vi har ett objekt som heter state och i det objektet ett som heter user annars visa <h2>Du har inget konto</h2> */}
+        { navigationState?.state?.user ? 
             <article> 
                 <p>{ navigationState.state.user.username }</p>
                 <p>{ navigationState.state.user.email }</p>
